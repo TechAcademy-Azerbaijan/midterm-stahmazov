40c4232475af76157cf748f439515c81e62639b4
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-06-21 19:24:30 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,23 @@
-const prompt = require('prompt');
-prompt.start();
+const num = n
+let count = 0;
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+while (n > 0) {
+    let num_ = num;
+    value = n % 10;
+    while (num_ > 0) {
+        console.log(value, num_%10)
+        if (value == (num_ % 10)) {
+            count++
+        }
+        num_=parseInt(num_/10)
+    }
+
+    n = parseInt(n/10)
+}
+
+if (count > 5) {
+    console.log('YES')
+} else {
+    console.log('NO')
+}
