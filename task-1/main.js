23f62f1dda6d9e644ef10a0d44978cd6c0e1161d
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-06-21 19:07:34 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,23 @@
+const { pbkdf2 } = require('crypto');
 const prompt = require('prompt');
 prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    const input = result.input; // "12,5"
+    const numbers = input.split(','); // ['12', '5']
+    let n = parseInt(numbers[0]); // Ilk daxil olunmuş ədədi alırıq. Nümunə - 12\
+
+    let sum = 0
+
+    while (n > 0) {
+        sum+=(n%10)
+        n = parseInt(n/10)
+    }
+    console.log(sum)
 });
